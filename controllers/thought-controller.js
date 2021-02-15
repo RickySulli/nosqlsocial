670c460ab7bbd6a383c4b53127f63010941395f7
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-02-14 22:13:20 -0600

    fixed bugs

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -96,7 +96,7 @@ const thoughtController = {
     deleteReaction({params}, res) {
         Thought.findOneAndUpdate(
             {_id: params.id},
-            {$pull:  {reactionId: params.reactionId}},
+            {$pull: { reactions: {reactionId: params.reactionId}}},
             {new:true, runValidators:true}
         )
         .then(dbThoughtData => {
